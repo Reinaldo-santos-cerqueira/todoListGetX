61aea5528f8f75b262909f36435fa025aebd5bff
Reinaldo Santos <reinaldosantos.hrzon@gmail.com>
2024-10-03 15:53:03 -0300

    refactor: mudando cores

--- lib/widgets/icons.dart
+++ lib/widgets/icons.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:getx_todo_list/app/core/values/icons.dart';
+import 'package:getx_todo_list/app/core/values/colors.dart';
 
 List<Icon> getIcons(){
  return const [
-  Icon(IconData(personIcon, fontFamily: 'MaterialIcons'),color: Colors.purple),
-  Icon(IconData(workIcon, fontFamily: 'MaterialIcons'),color: Colors.orange),
-  Icon(IconData(movieIcon, fontFamily: 'MaterialIcons'),color: Colors.green),
-  Icon(IconData(sportIcon, fontFamily: 'MaterialIcons'),color: Color.fromARGB(255, 255, 204, 0)),
-  Icon(IconData(travelIcon, fontFamily: 'MaterialIcons'),color: Colors.pink),
-  Icon(IconData(shopIcon, fontFamily: 'MaterialIcons'),color: Colors.blue),
+  Icon(IconData(personIcon, fontFamily: 'MaterialIcons'),color: purple),
+  Icon(IconData(workIcon, fontFamily: 'MaterialIcons'),color: pink),
+  Icon(IconData(movieIcon, fontFamily: 'MaterialIcons'),color: green),
+  Icon(IconData(sportIcon, fontFamily: 'MaterialIcons'),color: yellow),
+  Icon(IconData(travelIcon, fontFamily: 'MaterialIcons'),color: deepPink),
+  Icon(IconData(shopIcon, fontFamily: 'MaterialIcons'),color: blue),
  ];
 }
