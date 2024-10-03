681b7a03355050cb362d7c08dcd66d61e984acdb
Reinaldo Santos <reinaldosantos.hrzon@gmail.com>
2024-10-03 15:52:49 -0300

    refactor: change name color purble to purple

--- lib/app/core/values/colors.dart
+++ lib/app/core/values/colors.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-const purble = Color(0xFF756BFC);
+const purple = Color(0xFF756BFC);
 const pink = Color(0xFFF1A39A);
 const deepPink = Color(0xFFFA63C6);
 const green = Color(0xFF41CF9F);
