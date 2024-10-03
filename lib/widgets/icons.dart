fc91ef7bf566a3791407e7020af834fdf473c7be
Reinaldo Santos <reinaldosantos.hrzon@gmail.com>
2024-10-03 11:37:18 -0300

    feat: adicionando cor ao icone

--- lib/widgets/icons.dart
+++ lib/widgets/icons.dart
@@ -1,13 +1,13 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 import 'package:getx_todo_list/app/core/values/icons.dart';
 
 List<Icon> getIcons(){
  return const [
-  Icon(IconData(personIcon, fontFamily: 'MaterialIcons')),
-  Icon(IconData(workIcon, fontFamily: 'MaterialIcons')),
-  Icon(IconData(movieIcon, fontFamily: 'MaterialIcons')),
-  Icon(IconData(sportIcon, fontFamily: 'MaterialIcons')),
-  Icon(IconData(travelIcon, fontFamily: 'MaterialIcons')),
-  Icon(IconData(shopIcon, fontFamily: 'MaterialIcons')),
+  Icon(IconData(personIcon, fontFamily: 'MaterialIcons'),color: Colors.purple),
+  Icon(IconData(workIcon, fontFamily: 'MaterialIcons'),color: Colors.orange),
+  Icon(IconData(movieIcon, fontFamily: 'MaterialIcons'),color: Colors.green),
+  Icon(IconData(sportIcon, fontFamily: 'MaterialIcons'),color: Color.fromARGB(255, 255, 204, 0)),
+  Icon(IconData(travelIcon, fontFamily: 'MaterialIcons'),color: Colors.pink),
+  Icon(IconData(shopIcon, fontFamily: 'MaterialIcons'),color: Colors.blue),
  ];
 }
