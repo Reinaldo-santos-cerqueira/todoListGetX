b215377fadb1bd566669df0ef3912b45a7daf4d6
Reinaldo Santos <reinaldosantos.hrzon@gmail.com>
2024-10-04 17:33:43 -0300

    feat: adicionando elemento para representar as task em card

--- lib/modules/home/widgets/task_card.dart
+++ lib/modules/home/widgets/task_card.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:getx_todo_list/app/core/utils/extensions.dart';
 import 'package:getx_todo_list/data/models/task.dart';
+import 'package:getx_todo_list/modules/detail/view.dart';
 import 'package:getx_todo_list/modules/home/controller.dart';
 import 'package:step_progress_indicator/step_progress_indicator.dart';
 
@@ -15,68 +16,75 @@ class TaskCard extends StatelessWidget {
     final color = HexColor.fromHex(task.color);
     final squareWidth = Get.width - 12.0.wp;
     
-    return Container(
-      width: squareWidth / 2,
-      height: squareWidth / 2,
-      margin: EdgeInsets.all(3.0.wp),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey[300]!,
-            blurRadius: 7,
-            offset: const Offset(0, 7)
-          )
-        ]
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          StepProgressIndicator(
-            totalSteps: 100,
-            currentStep: 80,
-            size: 5,
-            padding: 0,
-            selectedGradientColor: LinearGradient(
-              colors: [color.withOpacity(.5),color],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
+    return GestureDetector(
+      onTap: (){
+        homeCtrl.changeTask(task);
+        homeCtrl.changeTodos(task.todos ?? []);
+        Get.to(()=>DetailsPage());
+      },
+      child: Container(
+        width: squareWidth / 2,
+        height: squareWidth / 2,
+        margin: EdgeInsets.all(3.0.wp),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey[300]!,
+              blurRadius: 7,
+              offset: const Offset(0, 7)
+            )
+          ]
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            StepProgressIndicator(
+              totalSteps: 100,
+              currentStep: 80,
+              size: 5,
+              padding: 0,
+              selectedGradientColor: LinearGradient(
+                colors: [color.withOpacity(.5),color],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+              unselectedGradientColor: const LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [Colors.white,Colors.white],
+              ),
             ),
-            unselectedGradientColor: const LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              colors: [Colors.white,Colors.white],
+            Padding(
+              padding: EdgeInsets.all(6.0.wp),
+              child: Icon(IconData(task.icon,fontFamily: 'MaterialIcons'),color: color),
             ),
-          ),
-          Padding(
-            padding: EdgeInsets.all(6.0.wp),
-            child: Icon(IconData(task.icon,fontFamily: 'MaterialIcons'),color: color),
-          ),
-          SizedBox(height: 2.0.wp,),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal:6.0.wp),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  task.title,
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 12.0.sp
+            SizedBox(height: 2.0.wp,),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal:6.0.wp),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    task.title,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 12.0.sp
+                    ),
+                    overflow: TextOverflow.ellipsis,
                   ),
-                  overflow: TextOverflow.ellipsis,
-                ),
-                Text(
-                  "${task.todos?.length ?? 0} Task",
-                  style: const TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: Colors.grey
-                  ),
-                )
-              ],
-            ),
-          )
-        ],
+                  Text(
+                    "${task.todos?.length ?? 0} Task",
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
